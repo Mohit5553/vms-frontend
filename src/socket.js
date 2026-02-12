c7d35a265104d2a652db2bd8898bf56e6008b121
MohitMaurya63 <mohitmaurya644@gmail.com>
2026-02-12 18:34:03 +0530

    a

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 // export const socket = io("http://localhost:5000");
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.env.production.VITE_API_URL;
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
