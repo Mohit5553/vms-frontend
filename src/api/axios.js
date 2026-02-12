c7a0a7ab9b034542626ffbc5db719a100a40f14d
MohitMaurya63 <mohitmaurya644@gmail.com>
2026-02-12 15:02:24 +0530

    n

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,14 +5,22 @@
 //   baseURL: "https://vms-backend-ctea.onrender.com/api",
 
 // });
+// import axios from "axios";
+
+// // ✅ FOR LOCAL
+// // const BASE_URL = "http://localhost:5000/api";
+
+// // ✅ FOR ONLINE
+// const BASE_URL = "https://vms-backend-ctea.onrender.com/api";
+
+// export default axios.create({
+//   baseURL: BASE_URL,
+// });
 import axios from "axios";
 
-// ✅ FOR LOCAL
-// const BASE_URL = "http://localhost:5000/api";
-
-// ✅ FOR ONLINE
-const BASE_URL = "https://vms-backend-ctea.onrender.com/api";
+const baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL,
 });
