a243b224b1046127842502ac9a18371ea2125dbb
MohitMaurya63 <mohitmaurya644@gmail.com>
2026-02-12 21:50:05 +0530

    a

--- src/api/axios.js
+++ src/api/axios.js
@@ -26,11 +26,19 @@
 // });
 import axios from "axios";
 
+// const API_URL = import.meta.env.VITE_API_URL;
+
+// const api = axios.create({
+//   baseURL: `${API_URL}/api`,
+//   withCredentials: true, // very important for login cookies
+// });
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
-  withCredentials: true, // very important for login cookies
+  withCredentials: true
 });
+
 
 export default api;
