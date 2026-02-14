6db87927acdeb395d10fc48f6819d57c57db75b8
MohitMaurya63 <mohitmaurya644@gmail.com>
2026-02-14 08:39:59 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 // src/config.js
 
-const BASE_URL = import.meta.env.VITE_API_URL;
-console.log("ENV VALUE:", import.meta.env.VITE_API_URL);
+export const API_BASE_URL =
+    import.meta.env.VITE_API_URL ||
+    "https://vms-backend-ctea.onrender.com/api";
 
-export const API_BASE_URL = `https://vms-backend-ctea.onrender.com/api`;
-export const SOCKET_BASE_URL = "https://vms-backend-ctea.onrender.com";
+export const SOCKET_BASE_URL =
+    import.meta.env.VITE_SOCKET_URL ||
+    "https://vms-backend-ctea.onrender.com";
